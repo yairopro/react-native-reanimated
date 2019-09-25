a58cadf01489eac9cce2e099c1ac182df1eebce3
Michał Osadnik <micosa97@gmail.com>
2019-09-25 13:43:14 +0200

    Revert "Update Example app to use RN 0.61 (#401)"
    This reverts commit 79dda5409cf5528741c322bfd93d8a2f1424633b.

--- Example/metro.config.js
+++ Example/metro.config.js
@@ -1,23 +1,13 @@
 const blacklist = require('metro-config/src/defaults/blacklist');
 const path = require('path');
+const cwd = path.resolve(__dirname);
 
 const glob = require('glob-to-regexp');
 
 function getBlacklist() {
   const nodeModuleDirs = [
-    glob(`${path.resolve(__dirname, '..')}/node_modules/*`),
-    glob(`${path.resolve(__dirname, '..')}/e2e/*`),
-    glob(`${path.resolve(__dirname)}/node_modules/*/node_modules/fbjs/*`),
-    glob(
-      `${path.resolve(
-        __dirname
-      )}/node_modules/*/node_modules/hoist-non-react-statics/*`
-    ),
-    glob(
-      `${path.resolve(
-        __dirname
-      )}/node_modules/react-native/node_modules/@babel/*`
-    ),
+    glob(`${process.platform === 'win32' ? path.resolve(__dirname, '..').replace(/\\/g, '/') : path.resolve(__dirname, '..')}/node_modules/*`),
+    glob(`${process.platform === 'win32' ? path.resolve(__dirname).replace(/\\/g, '/') : path.resolve(__dirname)}/node_modules/metro/node_modules/fbjs/*`),
   ];
   return blacklist(nodeModuleDirs);
 }
@@ -25,14 +15,7 @@ function getBlacklist() {
 module.exports = {
   resolver: {
     blacklistRE: getBlacklist(),
+    providesModuleNodeModules: ['react-native', 'react', 'fbjs'],
   },
   watchFolders: [path.resolve(__dirname, '..')],
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
 };
